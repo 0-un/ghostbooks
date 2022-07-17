7021fff235e35478a0e0d41eed3c6964cf812b47
0-un <0__un@naver.com>
2022-07-17 18:30:27 +0900

    js 수정

--- js/Main_Script.js
+++ js/Main_Script.js
@@ -92,30 +92,7 @@ var swiper = new Swiper(".sec6_carousel", {
   },
 });
 
-// ///////////////////////////////////////////////////////////////////////////
-// Hlight 스크롤시 하이라이트가 쳐지는 호버 //
 
-// function HlightTextWhy() {
-//   let hlight = document.querySelector(".sec2_content").offsetTop;
-//   let desc = document.querySelector(".sec2_main_text_description");
-//   hlight = hlight - 100;
-//   if (window.scrollY > hlight) {
-//     desc.classList.add("Axtivehlight");
-//   } else if (window.scrollY < hlight) {
-//     desc.classList.remove("Axtivehlight");
-//   }
-// }
-
-// window.onload = function () {
-//   //실행할 내용
-//   HlightTextWhy();
-// };
-
-// window.addEventListener("scroll", function () {
-//   HlightTextWhy();
-// });
-
-// ///////////////////////////////////////////////////////////////////////////
 // Smoke 효과 //
 
 function SmokeWithMe() {
@@ -199,25 +176,7 @@ function track(event) {
 }
 
 window.addEventListener("resize", track);
-// ///////////////////////////////////////////////////////////////////////////
-// 비디오 제어 스크립트 //
 
-// let mql = window.matchMedia("screen and (max-width: 768px)");
-// //윈도우 사이즈를 감지하여 선택적으로 코드 실행
-// window.addEventListener('resize', function(event) {
-// //윈도우 사이즈가 리사이즈 되었을 때
-// let video = document.querySelector('.sec4_video');
-
-// if (mql.matches) {
-//     // console.log("화면의 너비가 768px 미만.");
-//     video.pause();
-// } else {
-//     // console.log("화면의 너비가 768px 초과");
-//     video.play();
-// }
-// })
-
-// ///////////////////////////////////////////////////////////////////////////
 // sec5 체크박스 해제 및 메세지 전송 //
 
 let $check1 = document.getElementById("check1");
@@ -277,9 +236,3 @@ $msgButton.addEventListener("click", function () {
   }
 });
 
-// ///////////////////////////////////////////////////////////////////////////
-// window.onload = function () {
-//   //실행할 내용
-//   // SmokeWithMe();
-//   track();
-// };
